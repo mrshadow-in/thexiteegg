648d63aff270652976e61c5b15e4044e19ff8860
XD_UrDaD <70024979+XD-UrDaD@users.noreply.github.com>
2023-01-06 14:52:21 +0530

    Update hibernate.sh

--- hibernate.sh
+++ hibernate.sh
@@ -6,3 +6,5 @@ apt install wget
 
 wget https://github.com/XD-UrDaD/thexiteegg/raw/main/Hibernate-2.0.0.jar
 mv Hibernate-2.0.0.jar plugins/
+
+java -Xms256M  -XX:MaxRAMPercentage=80.0 -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar {{SERVER_JARFILE}}
